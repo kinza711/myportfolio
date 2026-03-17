cfef87d3b33795a92d4d65c267c27fa99b308b38
kinza <kinzashahzadi7181@gmail.com>
2026-03-17 23:39:08 +0500

    update: Personal chatbot integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import Home from "./pages/HomePage";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <div className="main">
+        <Home />
+        <iframe
+          src="https://personal-chatbotui.onrender.com/"
+          style={{
+            position: "fixed",
+            bottom: "20px",
+            right: "20px",
+            width: "350px",
+            height: "500px",
+            border: "none",
+            borderRadius: "20px",
+            zIndex: 9999,
+          }}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
